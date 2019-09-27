0faf51e7dc2075792e6a21543ff79e836347bedb
Zakaria Hasan <43992167+zakariahasan@users.noreply.github.com>
2019-09-27 08:04:04 +0300

    Update button.php

--- button.php
+++ button.php
@@ -1,49 +1,48 @@
 <?php 
-$file = "buttonStatus.txt";
-$handle = fopen($file,'w+');
+
 if (isset($_POST['S1on']))
 {
+	$file1 = "buttonStatus.txt";
+	$handle = fopen($file1,'w+');
 	
 	$onstring = "S1ON";
 	fwrite($handle,$onstring);
 	fclose($handle);
+	header('location:index.php');
 
-	print "
-		<html>
-
-			<head>
-				<title>IoT Led Test</title>
-			</head>
-
-
-			<body>
-				<h1>IoT LED Test</h1>
-				<h2>On button has been pressed. </h2>
-			</body>
-
-		</html> ";
 }
 
-
-
-
 else if (isset($_POST['S1off']))
-{
+{	
+	$file1 = "buttonStatus.txt";
+	$handle = fopen($file1,'w+');
+
 	$offstring = "S1OFF";
 	fwrite($handle, $offstring);
 	fclose($handle);
-	print"<html>
-
-		<head>
-	
-		<title>IoT Led Test</title>
-		
-		</head>
-
-		<body>
-
-			<h1>IoT LED Test</h1>
-			<h2>Off button has been pressed. </h2>
-		</body>
-		</html> ";
+	header('location:index.php');
  }
+
+else if (isset($_POST['S2on']))
+{
+	$file2 = "buttonStatus2.txt";
+	$handle = fopen($file2,'w+');
+
+	$offstring = "S1OFF";
+	fwrite($handle, $offstring);
+	fclose($handle);
+	header('location:index.php');	
+ }
+
+
+ else if (isset($_POST['S2off']))
+{	
+	$file2 = "buttonStatus2.txt";
+	$handle = fopen($file2,'w+');
+
+	$offstring = "S1OFF";
+	fwrite($handle, $offstring);
+	fclose($handle);
+	header('location:index.php');	
+ }
+ 
