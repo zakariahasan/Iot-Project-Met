8361b78259ae210b446668323b6ed4b8010e72ca
Zakaria Hasan <43992167+zakariahasan@users.noreply.github.com>
2019-09-28 06:59:37 +0100

    Update iot.py

--- iot.py
+++ iot.py
@@ -1,24 +1,36 @@
 import RPi.GPIO as GPIO
 import time
-import urllib2.request
-
+import dht11
 
 GPIO.setmode (GPIO.BCM)
-GPIO.setup(4,GPIO.OUT)
+tem= dht11.DHT11(pin=4)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+def temf():
+    time.sleep(2)
+    result= tem.read()
+    x=result.temperature
+    y=int(x)
+    if (y > 0):
+        with open("/var/www/html/read3.txt","w")as f3:
+            f3.write(str(y)+"C")
 
+def light():
+  with open("/var/www/html/buttonStatus.txt","r")as f1:
+    a=f1.read()
+    if (a=="S1ON"):
+         GPIO.output(17,GPIO.HIGH)
+    if (a=="S1OFF"):
+         GPIO.output(17,GPIO.LOW)
+def window():
+    with open("/var/www/html/buttonStatus2.txt","r")as f2:
+        b=f2.read()
+        if (b=="S2ON"):
+            GPIO.output(27,GPIO.HIGH)
+        if (b=="S2OFF"):
+            GPIO.output(27,GPIO.LOW)
 
 while True:
-
-	htmlfile = urllib2.request.urlopen(http://192.168.10.58/php)
-	htmltext = htmlfile.read ()
-	print (htmltext)
-
-	if (htmltext == b'S1ON'):
-		GPIO.output(4,GPIO.HIGH)
-		print ("LED ON")
-		time.sleep(1)
-
-	if (htmltext == b'S1OFF'):
-		GPIO.output(4,GPIOLOW)
-		print ("LED OFF")
-		time.sleep(1)
+    light()
+    window()
+    temf()
